9f986a486c2a84d1db73125a64d8025abd035ce3
modkit56 <siddharth.vagavolu@uwaterloo.ca>
2021-12-29 15:58:09 -0500

    Initial Commit

---
+++ tests/routes/root-route.test.js
@@ -0,0 +1,21 @@
+const build = require('../../src/app');
+
+let app;
+
+describe('Root Route', () => {
+  beforeEach(() => {
+    app = build();
+  });
+
+  afterEach(() => {
+    app.close();
+  });
+
+  it('should return 200 when the root route called', async () => {
+    const res = await app.inject({
+      url: '/',
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.json()).toEqual({ hello: 'world' });
+  });
+});
