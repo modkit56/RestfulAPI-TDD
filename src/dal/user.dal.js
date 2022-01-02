cc1368f4c5274dd60d19cb9ab2fe98595833241a
modkit56 <siddharth.vagavolu@uwaterloo.ca>
2022-01-02 18:57:54 -0500

    User Repository Layer
    - added users SQL schema
    - added migration script for above mentioned
    - added Repository Layer Level test cases

---
+++ src/dal/user.dal.js
@@ -0,0 +1,35 @@
+const userRepository = (db) => {
+  // validation not done here in this layer
+
+  // get user by user id
+  const getUserById = async (userId) => {
+    try {
+      return await db.one('select * from users where id = $1', [userId]);
+    } catch (error) {
+      throw Error(`${userId} does not exist`);
+    }
+  };
+
+  // save user in db
+  const saveUser = async (user) => {
+    try {
+      const { id } = await db.one(
+        'INSERT INTO users(first_name, middle_name, last_name, password, email) VALUES($1, $2, $3, $4, $5) RETURNING id',
+        [
+          user.first_name,
+          user.middle_name,
+          user.last_name,
+          user.password,
+          user.email,
+        ]
+      );
+      return id;
+    } catch (error) {
+      throw Error('Not Valid User Data');
+    }
+  };
+
+  return { getUserById, saveUser };
+};
+
+module.exports = userRepository;
---
+++ tests/dal/user.dal.test.js
@@ -0,0 +1,73 @@
+const Fastify = require('fastify');
+const userRepository = require('../../src/dal/user.dal');
+const dbPlugin = require('../../src/plugin/database');
+
+let app;
+
+describe('User Repository', () => {
+  beforeAll(async () => {
+    app = Fastify();
+    app.register(dbPlugin);
+    await app.ready();
+  });
+
+  // in case local container is running and we don't want persistant data
+  beforeEach(async () => {
+    await app.db.query('delete from users');
+  });
+
+  it('should save user in db', async () => {
+    const user = {
+      first_name: 'peter',
+      last_name: 'parker',
+      password: 'password',
+      email: 'email',
+    };
+
+    const { saveUser } = userRepository(app.db);
+
+    const userId = await saveUser(user);
+
+    expect(userId).toBeDefined();
+  });
+
+  it('should throw error when required filed is not present', async () => {
+    const user = {
+      first_name: 'peter',
+      last_name: 'parker',
+    };
+
+    const { saveUser } = userRepository(app.db);
+
+    await expect(saveUser(user)).rejects.toThrow(Error('Not Valid User Data'));
+  });
+
+  //
+
+  it('should return userId if it exists in db', async () => {
+    const user = {
+      first_name: 'peter',
+      last_name: 'parker',
+      password: 'password',
+      email: 'email',
+    };
+
+    const { saveUser, getUserById } = userRepository(app.db);
+
+    const userId = await saveUser(user);
+    // console.log(userId); to obtain dummy id for next test
+    const dbUser = await getUserById(userId);
+
+    expect(dbUser.first_name).toEqual('peter');
+    // expect(dbUser.lastName).toEqual('parker');
+    // expect(dbUser.password).toEqual('password');
+    // expect(dbUser.email).toEqual('email');
+  });
+
+  it('should throw expection if the user doesnt exist', async () => {
+    const { getUserById } = userRepository(app.db);
+    await expect(
+      getUserById('27ad34b0-45cf-43eb-ac65-a662175a1917')
+    ).rejects.toThrow('27ad34b0-45cf-43eb-ac65-a662175a1917 does not exist');
+  });
+});
